f8fcae6e0952f3e6cb189eb3ff1313e0e8d483dc
Pedro Santos <peusantos@gmail.com>
2020-04-23 13:27:26 -0300

    Updates

---
+++ precache-manifest.66fe84edf92892d13e2220f19e52a0b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c1a4d31eac3adeab9fc6237463231d",
+    "url": "/formClone/index.html"
+  },
+  {
+    "revision": "46c144569a20cca38403",
+    "url": "/formClone/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f01fbef289438a73c72e",
+    "url": "/formClone/static/js/2.0587a9a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/formClone/static/js/2.0587a9a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c144569a20cca38403",
+    "url": "/formClone/static/js/main.74139061.chunk.js"
+  },
+  {
+    "revision": "beff16eff6dd3d90b59a",
+    "url": "/formClone/static/js/runtime-main.f83f8817.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/formClone/static/media/logo.5d5d9eef.svg"
+  }
+]);
